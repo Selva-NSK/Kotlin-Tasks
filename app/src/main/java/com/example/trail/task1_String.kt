8e898693b8c3279ad18877611be1914cfdb736a8
Selva <kselvakumar@greatergoods.com>
2024-01-25 11:02:39 +0530

    Updated

--- app/src/main/java/com/example/trail/task1_String.kt
+++ app/src/main/java/com/example/trail/task1_String.kt
@@ -1,2 +1,15 @@
 package com.example.trail
-
+fun test(str : List<String>, key : String){
+    val stri: MutableSet<String> = mutableSetOf ()
+    for(word in str){
+        if(key in word){
+            stri.add (word)
+        }
+    }
+}
+fun main() {
+    val str: List<String> =
+        listOf("programming", "array", "kotlin", "algorithm", "java", "programming")
+    val ch = "ra"
+    test(str,ch)
+}
